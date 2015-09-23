48761ab2f56ce3f39d58bfb821db15466c678bf1
Jonatas Freitas <jonatasfreitasv@gmail.com>
2015-09-22 21:25:56 -0300

    Fix jasmine tests

---
+++ app/spec.js
@@ -0,0 +1,4 @@
+// This file is generated automatically.
+// All your modifications to it will be lost (so don't do it).
+import "./hello_world/hello_universe.spec.js";
+import "./hello_world/hello_world.spec.js";
