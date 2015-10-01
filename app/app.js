89250580140c593a4fe72d9b8bec19317ff5cd0e
Jonatas Freitas <jonatasfreitasv@gmail.com>
2015-10-01 20:18:18 -0300

    Added mustache.js dependence Celan code

--- app/app.js
+++ app/app.js
@@ -8,18 +8,31 @@ import { greet } from './hello_world/hello_world';
 // Node.js modules and those from npm
 // are required the same way as always.
 var os = require('os');
+
 var jetpack = require('fs-jetpack');
+var Mustache = require('mustache');
+window.jQuery = window.$ = require('jquery');
+window.Hammer = require('hammerjs');
 
 // Holy crap! This is browser window with HTML and stuff, but I can read
 // here files like it is node.js! Welcome to Electron world :)
 var manifest = jetpack.read('package.json', 'json');
 console.log(manifest);
 
-// window.env contains data from config/env_XXX.json file.
-var envName = window.env.name;
-
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('greet').innerHTML = greet();
-    document.getElementById('platform-info').innerHTML = os.platform();
-    document.getElementById('env-name').innerHTML = envName;
+
+    var template = $('#template').html();
+    Mustache.parse(template);
+
+    var view = {
+        greet: greet(),
+        platform: os.platform(),
+        envName: null
+    };
+
+    var rendered = Mustache.render(template, view);
+    $('#target').html(rendered);
+
+    Materialize.toast('I am a toast!', 4000);
+
 });
